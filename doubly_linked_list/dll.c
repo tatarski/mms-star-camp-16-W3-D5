08dbba9a0cdf1d0f14b2ba53b86151f2249955c7
tatarski1234 <tatarski.dim@gmail.com>
2022-08-05 17:14:40 +0300

    Add double linked list functions

--- doubly_linked_list/dll.c
+++ doubly_linked_list/dll.c
@@ -1,31 +1,57 @@
 #include "dll.h"
 
-void create_example_dll()
-{
-    test_data[0] = (Data){100, 12.3};
-    test_data[1] = (Data){126, 12.3};
-    test_data[2] = (Data){305, 12.3};
-    test_data[3] = (Data){23, 12.3};
-    test_data[4] = (Data){55, 12.3};
-    test_nodes[0] = (Node){test_data[0], NULL, test_nodes + 1};
-    test_nodes[1] = (Node){test_data[1], test_nodes, test_nodes + 2};
-    test_nodes[2] = (Node){test_data[2], test_nodes + 1, test_nodes + 3};
-    test_nodes[3] = (Node){test_data[3], test_nodes + 2, test_nodes + 4};
-    test_nodes[4] = (Node){test_data[4], test_nodes + 3, NULL};
-    test_DLL.head = test_nodes;
-    test_DLL.tail = test_nodes + 4;
-}
-
 void print_dll(DLL dll)
 {
+    if(!dll.head && !dll.tail) {
+        printf("Empty double linked list\n");
+        return;
+    }
+    Node *cur_node_pointer = dll.head;
+    while(cur_node_pointer != NULL){
+        printf("prev: %15p, timestamp:%4lu, temp:%f, addr: %15p, next: %p\n",
+            cur_node_pointer->prev, cur_node_pointer->data.timestamp,
+            cur_node_pointer->data.temperature, cur_node_pointer, cur_node_pointer->next);
+        cur_node_pointer = cur_node_pointer->next;
+    }
 }
 
 void push_back(DLL *dll, Data new_data)
 {
+    // Allocate memory for new node
+    Node *new_node = malloc(sizeof(Node));
+    if(!new_node) {perror("Malloc failed\n"); exit(-1);}
+    new_node->data = new_data;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    // Linked list had 0 elements
+    if(!dll->head && !dll->tail) {
+        dll->head = new_node;
+        dll->tail = new_node;
+        return;
+    }
+
+    new_node->prev = dll->tail;
+    dll->tail->next = new_node;
+    dll->tail = new_node;
 }
 
 void pop_front(DLL *dll)
 {
+    Node* deleted_node = dll->head;
+    if(!dll->head && !dll->tail) {
+        perror("Empty linked list\n");
+        exit(-1);
+    }
+    // Linked list has 1 element
+    if(dll->head == dll->tail) {
+        free(deleted_node);
+        dll->head = NULL;
+        dll->tail = NULL;
+        return;
+    }
+    dll->head = dll->head->next;
+    dll->head->prev = NULL;
+    free(deleted_node);
 }
 
 void pop_back(DLL *dll)
